f9a620d7119d5839d49c27183943adef7d80693c
fahd-111 <fahad.jcat123@gmail.com>
2025-02-09 18:12:09 +0500

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <ConfigProvider theme={antDesignThemeConfig}>
       <div className="w-screen bg-black min-h-screen flex flex-col">
-        <div className="w-full max-w-[900px] flex flex-col items-start justify-start gap-5 mx-auto  pb-20 px-4">
+        <div className="w-full max-w-[900px] flex flex-col items-start justify-start gap-5 mx-auto py-2 pb-20 px-4">
           <div className="w-full mt-10">
             <InteractiveAvatar />
           </div>
